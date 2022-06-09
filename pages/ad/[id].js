24abf45c37a547fce99f2e06315ab5856759e4b5
Ilyar <ilyarsavutov@gmail.com>
2022-06-09 18:54:44 +0600

    9

--- pages/ad/[id].js
+++ pages/ad/[id].js
@@ -5,7 +5,7 @@ const AdItem = () => {
         <div className="row fs-5 border-bottom pb-4">
           Toyota Camry 2007 года
         </div>
-        <div className="row mt-3">
+        <div className="row mt-3 gx-5">
           <div className="col-4 fs-5">
             <div className="row mb-3">
               <span>6 300 000 ₸</span>
@@ -29,66 +29,86 @@ const AdItem = () => {
             <div className="row mb-3">
               <button className="btn btn-warning">Рассчитать кредит</button>
             </div>
-            <div className="row">
+            <div className="row mb-2">
               <div className="col p-0 fs-6">
                 <span>Город</span>
               </div>
-              <div className="col fs-6">Актау</div>
+              <div className="col fs-6">
+                <span>Актау</span>
+              </div>
             </div>
-            <div className="row">
+            <div className="row mb-2">
               <div className="col p-0 fs-6">
                 <span>Кузов</span>
               </div>
-              <div className="col fs-6">седан</div>
+              <div className="col fs-6">
+                <span>седан</span>
+              </div>
             </div>
-            <div className="row">
+            <div className="row mb-2">
               <div className="col p-0 fs-6">
                 <span>Объем двигателя, л</span>
               </div>
-              <div className="col fs-6">2.4 (бензин)</div>
+              <div className="col fs-6">
+                <span>2.4 (бензин)</span>
+              </div>
             </div>
-            <div className="row">
+            <div className="row mb-2">
               <div className="col p-0 fs-6">
                 <span>Пробег</span>
               </div>
-              <div className="col fs-6">287 478 км</div>
+              <div className="col fs-6">
+                <span>287 478 км</span>
+              </div>
             </div>
-            <div className="row">
+            <div className="row mb-2">
               <div className="col p-0 fs-6">
                 <span>Коробка передач</span>
               </div>
-              <div className="col fs-6">автомат</div>
+              <div className="col fs-6">
+                <span>автомат</span>
+              </div>
             </div>
-            <div className="row">
+            <div className="row mb-2">
               <div className="col p-0 fs-6">
                 <span>Руль</span>
               </div>
-              <div className="col fs-6">слева</div>
+              <div className="col fs-6">
+                <span>слева</span>
+              </div>
             </div>
-            <div className="row">
+            <div className="row mb-2">
               <div className="col p-0 fs-6">
                 <span>Цвет</span>
               </div>
-              <div className="col fs-6">серый металлик</div>
+              <div className="col fs-6">
+                <span>серый металлик</span>
+              </div>
             </div>
-            <div className="row">
+            <div className="row mb-2">
               <div className="col p-0 fs-6">
                 <span>Привод</span>
               </div>
-              <div className="col fs-6">передний привод</div>
+              <div className="col fs-6">
+                <span>передний привод</span>
+              </div>
             </div>
-            <div className="row">
+            <div className="row mb-2">
               <div className="col p-0 fs-6">
                 <span>Растаможен в Казахстане</span>
               </div>
-              <div className="col fs-6">Да</div>
+              <div className="col fs-6">
+                <span>Да</span>
+              </div>
             </div>
-            <div className="row">
+            <div className="row mb-2">
               <div className="col p-0 fs-6">
                 <span>VIN</span>
               </div>
-              <div className="col fs-6">4XXXXXXX*XX****XX</div>
-            </div>  
+              <div className="col fs-6">
+                <span>4XXXXXXX*XX****XX</span>
+              </div>
+            </div>
           </div>
           <div className="col-8">
             <div className="row">
